af3bf5452b091af0fea6b4662dcbe9ff44f621fe
Carlos Moreno <omorenodovale@gmail.com>
2020-02-18 20:15:17 -0500

    Create __init__ class Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
-    def cumprimentar(self):
-        return f'Olá {id(self)}'
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
 
-if __name__ == '__main__':
-    p = Pessoa()
+    def cumprimentar(self):
+        return f"Olá {id(self)}"
+
+
+if __name__ == "__main__":
+    p = Pessoa('Carlos', 30)
+    print(f'{p.nome} - {p.idade}')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
