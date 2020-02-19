1d8db1e0105a4c77ffd67a3fac72940e3fcbae43
Carlos Moreno <omorenodovale@gmail.com>
2020-02-18 20:29:29 -0500

    Created complex attribute

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
 
 if __name__ == "__main__":
-    p = Pessoa('Carlos', 30)
-    print(f'{p.nome} - {p.idade}')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    isabella = Pessoa(nome='Isabella')
+    luiz = Pessoa(nome='Luíz')
+    carlos = Pessoa(isabella, luiz, nome='Carlos', idade=30)
+    print(f'{carlos.nome} - {carlos.idade}')
+    print(id(carlos))
+    for children in carlos.filhos:
+        print(f'{id(children)}: {children.nome}')
