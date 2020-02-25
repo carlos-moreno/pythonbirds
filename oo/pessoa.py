c08401e411c0fd90f48a70b2d561c7147fcb6530
Carlos Moreno <omorenodovale@gmail.com>
2020-02-24 23:15:23 -0500

    Created class attribute.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
@@ -9,18 +11,23 @@ class Pessoa:
 
 
 if __name__ == "__main__":
-    isabella = Pessoa(nome='Isabella')
-    luiz = Pessoa(nome='Luíz')
-    carlos = Pessoa(isabella, luiz, nome='Carlos', idade=30)
-    print(f'{carlos.nome} - {carlos.idade}')
+    isabella = Pessoa(nome="Isabella")
+    luiz = Pessoa(nome="Luíz")
+    carlos = Pessoa(isabella, luiz, nome="Carlos", idade=30)
+    print(f"{carlos.nome} - {carlos.idade}")
     print(id(carlos))
     for children in carlos.filhos:
-        print(f'{id(children)}: {children.nome}')
+        print(f"{id(children)}: {children.nome}")
 
-    carlos.sobrenome = 'Moreno'
+    carlos.sobrenome = "Moreno"
     print(carlos.sobrenome)
     print(carlos.__dict__)
     print(isabella.__dict__)
     print(luiz.__dict__)
     del carlos.sobrenome
     print(carlos.__dict__)
+    print(Pessoa.olhos)
+    print(luiz.olhos)
+    print(isabella.olhos)
+    carlos.olhos = 1
+    print(carlos.olhos)
