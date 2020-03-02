59ba1ccddcb690a1edd42c9cb19d88d815114dad
Carlos Moreno <omorenodovale@gmail.com>
2020-03-02 16:19:32 -0500

    Update class Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,42 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
+    @staticmethod
+    def metodo_statico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f"{cls} - olhos {cls.olhos}"
+
 
 if __name__ == "__main__":
-    p = Pessoa('Carlos', 30)
-    print(f'{p.nome} - {p.idade}')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    isabella = Pessoa(nome="Isabella")
+    luiz = Pessoa(nome="Luíz")
+    carlos = Pessoa(isabella, luiz, nome="Carlos", idade=30)
+    print(f"{carlos.nome} - {carlos.idade}")
+    print(id(carlos))
+    for children in carlos.filhos:
+        print(f"{id(children)}: {children.nome}")
+
+    carlos.sobrenome = "Moreno"
+    print(carlos.sobrenome)
+    print(carlos.__dict__)
+    print(isabella.__dict__)
+    print(luiz.__dict__)
+    del carlos.sobrenome
+    print(carlos.__dict__)
+    print(Pessoa.olhos)
+    print(luiz.olhos, luiz.olhos)
+    carlos.olhos = 1
+    print(carlos.olhos)
+    print(carlos.metodo_statico(), Pessoa.metodo_statico())
+    print(carlos.nome_e_atributos_de_classe(), Pessoa.nome_e_atributos_de_classe())
